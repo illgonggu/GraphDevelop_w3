77142bef6db6e140b5f34d5e5d4eeedf5b301f43
illgonggu <rbfl2365@icloud.com>
2022-11-11 21:15:33 +0900

    Initial Commit

---
+++ GraphDevelop_w3/GraphDevelop_w3App.swift
@@ -0,0 +1,17 @@
+//
+//  GraphDevelop_w3App.swift
+//  GraphDevelop_w3
+//
+//  Created by Loopy on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct GraphDevelop_w3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
